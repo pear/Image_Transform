7d4a1fdf9c2a9c372f0332ac9a06ea7e3664aed8
Alan Knowles <alan_k@php.net>
2003-07-05 08:16:45 +0000

    correctly fix the ImageCreate/Copy test
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_Transform/trunk@134215 c90b9560-bf6c-de11-be94-00142212c4b1

--- Driver/GD.php
+++ Driver/GD.php
@@ -330,12 +330,12 @@ Class Image_Transform_Driver_GD extends Image_Transform
         if ($this->resized === true) {
             return PEAR::raiseError('You have already resized the image without saving it.  Your previous resizing will be overwritten', null, PEAR_ERROR_TRIGGER, E_USER_NOTICE);
         }
-        if (function_exists('ImageCreateTrueColor')) {
+        if (function_exists('ImageCreateTrueColor') && @ImageCreateTrueColor()) {
             $new_img =ImageCreateTrueColor($new_x,$new_y);
         } else {
             $new_img =ImageCreate($new_x,$new_y);
         }
-        if (function_exists('ImageCopyResampled')) {
+        if (function_exists('ImageCopyResampled') && @ImageCopyResampled()) {
             ImageCopyResampled($new_img, $this->imageHandle, 0, 0, 0, 0, $new_x, $new_y, $this->img_x, $this->img_y);
         } else {
             ImageCopyResized($new_img, $this->imageHandle, 0, 0, 0, 0, $new_x, $new_y, $this->img_x, $this->img_y);
