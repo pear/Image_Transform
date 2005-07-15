f4ccd78846ea75ba0bb37e94566ccdfb837368b1
Philippe Jausions <jausions@php.net>
2005-07-15 05:18:42 +0000

    Updated header block
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_Transform/trunk@190728 c90b9560-bf6c-de11-be94-00142212c4b1

--- Driver/Imagick.php
+++ Driver/Imagick.php
@@ -1,30 +1,43 @@
 <?php
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Peter Bowyer <peter@mapledesign.co.uk>                      |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
-// Image Transformation interface using old ImageMagick extension
-// DEPRECIATED - current CVS/release imagick extension should use 
-// the Imagick2 driver.
-//
 
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * Image Transformation interface using old ImageMagick extension
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Image
+ * @package    Image_Transform
+ * @author     Peter Bowyer <peter@mapledesign.co.uk>
+ * @copyright  2002-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @deprecated
+ * @link       http://pear.php.net/package/Image_Transform
+ */
+
+/**
+ * Include of base class
+ */
 require_once "Image/Transform.php";
 
-Class Image_Transform_Driver_Imagick extends Image_Transform
+
+/**
+ * Image Transformation interface using old ImageMagick extension
+ *
+ * DEPRECATED: current CVS/release imagick extension should use
+ * the Imagick2 driver
+ *
+ * @deprecated
+ */
+class Image_Transform_Driver_Imagick extends Image_Transform
 {
     /**
      * Handler of the imagick image ressource
